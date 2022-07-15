f8e6eae4e6cccc7f59d17c8dd5a3959edb384fb5
sheep <82494123+thejanjan@users.noreply.github.com>
2022-07-15 10:52:09 -0700

    Game: Introduce base scenes with grid manager

---
+++ game/GridManager.gd
@@ -0,0 +1,24 @@
+extends Node2D
+
+"""
+A manager class in charge of keeping the entire game grid maintained.
+"""
+
+
+var game_grid = [];
+export var width = 10;
+export var height = 10;
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	for i in range(width):
+		var row = []
+		for l in range(height):
+			row.append(0)
+		self.game_grid.append(row)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
