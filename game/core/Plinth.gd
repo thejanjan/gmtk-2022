d4eae34dfc5c43013db97ba47ae42c1bbcc8a791
chasminimus <chas.kuch@gmail.com>
2022-07-16 01:06:08 -0500

    add a test plinth to the world lol
    lol! lmao!

--- game/core/Plinth.gd
+++ game/core/Plinth.gd
@@ -4,6 +4,7 @@ extends Area2D
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
+export var cost: int = 0
 
 
 # Called when the node enters the scene tree for the first time.
@@ -15,6 +16,6 @@ func _ready():
 #func _process(delta):
 #	pass
 
-
-func _on_Plinth_body_entered(body:Node):
+# should only be called when the player enters the collision range
+func _on_Plinth_body_entered(_body: Node):
 	print("WANNA BU Y SOMETHING")
