04f2279d44916a6a5c06d5086e081c8f63ee0a8a
RNLFoof <z.zablotsky@gmail.com>
2022-07-16 00:08:36 -0400

    WHY DOES IT THINK I MADE ALL THESE FILES

--- game/core/player/PlayerControl.gd
+++ game/core/player/PlayerControl.gd
@@ -2,11 +2,12 @@ extends KinematicBody2D
 
 const Item = preload("res://game/core/Item.gd")
 const PlayerStats = preload("res://game/core/player/PlayerStats.gd")
+onready var ConcreteStream: AudioStreamPlayer = $ConcreteStream;
 
 var _stats = PlayerStats.new()
 var velocity: Vector2;
 # The higher these are, the slower the speed changes to nothing/max speed respectively
-var friction = 60;
+var friction = 10;
 var acceleration = 90;
 
 onready var PlayerSprite = $PlayerSprite
@@ -16,11 +17,13 @@ func _ready():
 	_stats._speed = 900;
 	_stats._damage = 1;
 	velocity = Vector2(0,0);
-	pass # Replace with function body.
+	print(ConcreteStream.autoplay);
+	pass # Replace with function bitches instead.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
+	# Movement
 	var h_move = int(Input.is_action_pressed("move_right")) - int(Input.is_action_pressed("move_left"))
 	var v_move = int(Input.is_action_pressed("move_down")) - int(Input.is_action_pressed("move_up"))
 	
@@ -36,6 +39,18 @@ func _physics_process(delta):
 				velocity[i] = movetowardsmaxspeed;
 			else:
 				velocity[i] = movetowardszero;
+	
+	# Concrete
+	var totalspeed = abs(velocity[0] + velocity[1]);
+	if totalspeed <= 1:
+		ConcreteStream.volume_db = -80;
+		ConcreteStream.playing = false;
+	else:
+		var minvol = -20;			
+		var maxvol = 20;			
+		ConcreteStream.volume_db = minvol + (abs(minvol)+abs(maxvol)) * min(_stats._speed, abs(velocity[0] + velocity[1])) / _stats._speed;
+		if not ConcreteStream.playing:
+			ConcreteStream.playing = true;
 
 	velocity = move_and_slide(velocity)
 	for i in get_slide_count():
