21aa56dc17279713afaf3c58fdcb03efeec44de1
Paul C <paul.luap09@gmail.com>
2022-07-15 20:58:21 -0400

    Let the player's speed be influenced by items

--- game/core/player/PlayerControl.gd
+++ game/core/player/PlayerControl.gd
@@ -1,13 +1,14 @@
 extends Sprite
 
+var Item = load("res://game/core/Item.gd")
+var PlayerStats = load("res://game/core/player/PlayerStats.gd")
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
+var _stats = PlayerStats.new()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	_stats._speed = 3;
+	_stats._damage = 1;
 	pass # Replace with function body.
 
 
@@ -16,4 +17,8 @@ func _physics_process(delta):
 	var h_move = int(Input.is_action_pressed("move_right")) - int(Input.is_action_pressed("move_left"))
 	var v_move = int(Input.is_action_pressed("move_down")) - int(Input.is_action_pressed("move_up"))
 	
-	self.translate(Vector2(h_move * 3, v_move * 3))
+	self.translate(Vector2(h_move * _stats._speed, v_move * _stats._speed))
+
+func apply_item(item):
+	_stats._speed += item._stats._speed
+	_stats._damage += item._stats._damage
