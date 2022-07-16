7ba33d3205903543e790602f262db3514909d570
Paul C <paul.luap09@gmail.com>
2022-07-16 04:53:56 -0400

    It begins (sort of)

---
+++ game/ABANDON ALL HOPE/DungeonGenerator.gd
@@ -0,0 +1,19 @@
+# Even though a red miasma floods the air, you can still see the sky.
+# Even though concrete roars in your ears, you can still feel your own breath.
+# Even though peril surrounds you, you can still wear your smile.
+
+# This isn't hell.
+
+extends Node
+
+
+# Called when the node enters the... oh, you know that already.
+func _ready():
+	pass
+
+# Delaunay triangulation brings forth the power of the simplex.
+# Do you know what else holds the power of the simplex?
+# Yes, that's right.
+# Herpes simplex virus.
+func triangulate():
+	pass
