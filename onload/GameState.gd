b6d9bda1d7c6df288d9ee6c2ab0ed2fbe3f621b4
sheep <82494123+thejanjan@users.noreply.github.com>
2022-07-15 19:30:26 -0700

    GameState: Autload on another planet

---
+++ onload/GameState.gd
@@ -0,0 +1,18 @@
+extends Node
+
+
+"""
+An autoloaded script that is in charge of
+preserving the game's current state (player HP, equipment, etc)
+"""
+
+
+var HP = 6 
+var Equipment = {
+	Enum.DiceSide.ONE: null,
+	Enum.DiceSide.TWO: null,
+	Enum.DiceSide.THREE: null,
+	Enum.DiceSide.FOUR: null,
+	Enum.DiceSide.FIVE: null,
+	Enum.DiceSide.SIx: null,
+}
