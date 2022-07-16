ab212ac88452ce5d828661306028c70db22c6eb0
chasminimus <chas.kuch@gmail.com>
2022-07-16 18:16:06 -0500

    Plinths now pull item sprites from the DB

--- game/core/Plinth.gd
+++ game/core/Plinth.gd
@@ -1,16 +1,28 @@
 extends Area2D
 
+export(int) var cost = 0
+export(Enum.ItemType) var item_type = Enum.ItemType.NIL
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-export var cost: int = 0
-
+onready var item_sprite = $ItemSprite
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	# if an item isn't manually set, pull from the pool
+	if item_type == Enum.ItemType.NIL:
+		print("Populating Plinth")
+		populate()
+	else:
+		set_item_type(item_type)
 
+func set_item_type(new_item_type: int):
+	item_type = new_item_type
+	var item_data = Database.get_item_data(item_type)
+	if new_item_type != Enum.ItemType.NIL:
+		item_sprite.set_texture(item_data.get_texture())
+
+func populate():
+	var item_data: Database._ItemData = ItemPool.pop_random_item()
+	set_item_type(item_data.get_item_type())
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
