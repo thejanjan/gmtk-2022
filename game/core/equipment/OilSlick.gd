4312d29e5fb475d02f8d359fcaddf76aaf1d6e83
RNLFoof <z.zablotsky@gmail.com>
2022-07-17 01:00:04 -0400

    Very good change

--- game/core/equipment/OilSlick.gd
+++ game/core/equipment/OilSlick.gd
@@ -5,6 +5,7 @@ func enter():
 	tempStatChange("_speed", 3, 15)
 	tempStatChange("_acceleration", 10, 15)
 	tempStatChange("_friction", 20, 15)
+	changeConcreteSound("res://audio/lancersplat.wav")
 
 func _physics_process(delta):
 	createTrail(1, 2, Color("#1a1a1f"))
