d5c4ca1e8beaf4a16a4ba3a5528f66d82c02cf89
sheep <82494123+thejanjan@users.noreply.github.com>
2022-07-16 23:15:29 -0700

    Equipment: Adjust various buffs

--- game/core/equipment/OilSlick.gd
+++ game/core/equipment/OilSlick.gd
@@ -1,11 +1,24 @@
 class_name OilSlick
 extends Equipment
 
+
 func enter():
-	tempStatChange("_speed", 3, 15)
-	tempStatChange("_acceleration", 10, 15)
-	tempStatChange("_friction", 20, 15)
+	self.apply_stats(false)
 	changeConcreteSound("res://audio/lancersplat.wav")
+	
+func apply_stats(invert: bool):
+	if not invert:
+		_statChange("_speed", 3.0)
+		_statChange("_acceleration", 10.0)
+		_statChange("_friction", 20.0)
+	else:
+		_statChange("_speed", 1.0 / 3.0)
+		_statChange("_acceleration", 1.0 / 10.0)
+		_statChange("_friction", 1.0 / 20.0)
+	
+func exit():
+	self.apply_stats(true)
+	changeConcreteSound("res://audio/concrete.ogg")
 
 func _physics_process(delta):
 	createTrail(1, 2, Color("#1a1a1f"))
